fbd10882c42012ce81c61649215de072b14ec856
Sayan Biswas <sayan@pokurt.me>
2021-06-13 12:49:40 +0530

    fix headers a bit.

--- coffeehouse/key.go
+++ coffeehouse/key.go
@@ -1,5 +1,5 @@
 /*
- * This file is part of Intellivoid.Coffeehouse-go (https://github.com/Intellivoid/Intellivoid.Coffeehouse-go).
+ * This file is part of Intellivoid.Coffeehouse-go (https://github.com/Dank-del/Intellivoid.Coffeehouse-go).
  * Copyright (c) 2021 Sayan Biswas.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package coffeehouse
 
 
